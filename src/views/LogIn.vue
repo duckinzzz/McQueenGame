543c466002667ccd414610bfce964b73c1c94179
darksquare1 <nekit05@list.ru>
2023-11-24 08:34:35 +0300

    added vue login with jwt authorize

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="log-in-page">
     <h1>Log in</h1>
+    <form @submit.prevent="submitForm">
+      <label>Username:</label>
+      <input type="email" name="username" v-model="username"><br><br>
+      <label>Password:</label>
+      <input type="password" name="password" v-model="password"><br><br>
+      <button type="sumbit">Log in</button>
+    </form>
   </div>
 </template>
 <script>
@@ -8,6 +15,30 @@ import axios from 'axios'
 
 export default {
   name: "LogIn",
+  data(){
+    return{
+      username: '',
+      password: ''
+    }
+  },
+  methods:{
+    submitForm(e){
+      axios.defaults.headers.common['Authorization']= ''
+      localStorage.removeItem("access")
+      const formData = {
+        username: this.username,
+        password: this.password
+      }
+      axios
+          .post('api/v1/jwt/create/',formData)
+          .then(response =>{
+            console.log(response)
+          })
+          .catch(error =>{
+            console.log(error)
+          })
+    }
+  }
 }
 
 </script>
