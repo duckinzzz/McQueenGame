f5f710a21dc76e7a42bccbca24a1a50bd05c92b0
darksquare1 <nekit05@list.ru>
2023-11-24 09:51:06 +0300

    homeview fixed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,37 @@
 <template>
   <div class="home">
-    <h1></h1>
+    <h1>Home</h1>
+
+    <span :src ="user_data">{{user_data}}</span>
   </div>
 </template>
 
 <script lang="ts">
+import axios from "axios";
+import { defineComponent } from 'vue';
 
-export default {
+export default defineComponent({
   name: 'HomeView',
-};
+  data(){
+    return{
+      user_data: '' as string
+    }
+  },
+  mounted() {
+    this.getMe()
+  },
+  methods:{
+    getMe(): void {
+      axios
+          .get("api/v1/users/me")
+          .then(response => {
+            console.log(response)
+            this.user_data = response.data.username
+          })
+          .catch(error => {
+            console.log(error)
+          })
+    }
+  }
+});
 </script>
