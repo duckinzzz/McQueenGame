0fb65da326a42970e12e4165e742a9dc76751bbf
cyberciderrr <136062393+cyberciderrr@users.noreply.github.com>
2023-12-04 16:28:34 +1000

    added update_record_api_view

--- records/views.py
+++ records/views.py
@@ -1,12 +1,22 @@
-from django.shortcuts import render
-from .models import Record
 
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+
+from .models import Record
 from .serializers import RecordSerializer
 
 
-class RecordApiList(generics.ListAPIView):
+class RecordApiList(generics.ListCreateAPIView):
     queryset = Record.objects.all()
     serializer_class = RecordSerializer
     permission_classes = (IsAuthenticatedOrReadOnly,)
+
+
+class RecordChangeApi(generics.UpdateAPIView):
+    queryset = Record.objects.all()
+    serializer_class = RecordSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+
