3771428440866160c12aa980979f97dd6fee35f7
darksquare1 <132488469+darksquare1@users.noreply.github.com>
2023-12-05 12:18:03 +0300

    order records by record descending order

--- records/views.py
+++ records/views.py
@@ -1,12 +1,11 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .models import Record
 from .serializers import RecordSerializer
 
 
 class RecordApiList(generics.ListCreateAPIView):
-    queryset = Record.objects.all()
+    queryset = Record.objects.all().order_by('-record')
     serializer_class = RecordSerializer
 
 
