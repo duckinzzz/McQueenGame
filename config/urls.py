3f479e6e07a7138b8521b4a18c29064c3630b5fe
darksquare1 <132488469+darksquare1@users.noreply.github.com>
2023-12-05 09:18:07 +0300

    chandged urls.py

--- config/urls.py
+++ config/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('api/v1/', include('djoser.urls')),
     path('api/v1/', include('djoser.urls.jwt')),
     path('api/v1/records/', RecordApiList.as_view(), name='records_list'),
-    path('api/v1/update_record/', RecordChangeApi.as_view(), name='record_update'),
+    path('api/v1/update_record/<str:user__username>/', RecordChangeApi.as_view(), name='record_update'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
