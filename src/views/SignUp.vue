be9e21755534980fba04dba5682b863e3d5bb0fa
darksquare1 <132488469+darksquare1@users.noreply.github.com>
2023-12-05 09:23:31 +0300

    set nickname in store after authorize

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -45,12 +45,14 @@ export default {
             console.log(response);
             const access = response.data.access;
             const refresh = response.data.refresh;
-
-            localStorage.setItem("access", access);
-            localStorage.setItem("refresh", refresh);
-
+            const username = this.username
+            this.$store.commit("setUserName", username);
+            this.$store.commit("setAccess", access);
+            this.$store.commit("setRefresh", refresh);
 
             axios.defaults.headers.common['Authorization'] = "JWT " + access;
+            localStorage.setItem("access", access);
+            localStorage.setItem("refresh", refresh);
 
             router.push('/');
           })
